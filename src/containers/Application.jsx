a527e8d7e965c3fdf1433537c9f568229e32a538
Mian Uddin <mianuddin@gmail.com>
2018-12-21 00:07:34 -0800

    feat(Application.jsx): build out first row of application and other functionality

--- src/containers/Application.jsx
+++ src/containers/Application.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import { fetchApplications } from '../actions/index';
 
 class Application extends Component {
@@ -14,17 +17,105 @@ class Application extends Component {
   }
 
   render() {
-    const { appData, errorMessage } = this.props;
-    if (appData) {
+    const { appData, errorMessage, fetching } = this.props;
+
+    const tempCardStyle = {
+      display: 'inline-block',
+      minWidth: 275,
+      marginRight: '1rem',
+      marginBottom: '1rem',
+    };
+
+    const rowStyle = {
+      display: 'flex',
+      flexWrap: 'wrap',
+      alignItems: 'flex-start',
+    };
+
+    if (fetching) {
       return (
         <div>
-          {appData.name}
+          <CircularProgress />
         </div>
       );
     }
+
+    if (appData) {
+      return (
+        <div>
+          <h1>{appData.name}</h1>
+          <section>
+            <h2>Personal Info</h2>
+            <div style={rowStyle}>
+              <Card style={tempCardStyle}>
+                <CardContent>
+                  <h3>Demographics</h3>
+                  <p>
+                    Graduation Date:
+                    {` ${appData.grad_date}`}
+                  </p>
+                  <p>
+                    Gender:
+                    {` ${appData.gender ? appData.gender : appData.other_gender}`}
+                  </p>
+                  <p>
+                    Ethnicity:
+                    {` ${appData.ethnicity ? appData.ethnicity : appData.other_ethnicity}`}
+                  </p>
+                </CardContent>
+              </Card>
+              <Card style={tempCardStyle}>
+                <CardContent>
+                  <h3>Travel</h3>
+                  <p>
+                    College:
+                    {` ${appData.college}`}
+                  </p>
+                  <p>
+                    City:
+                    {` ${appData.city}`}
+                  </p>
+                </CardContent>
+              </Card>
+              <Card style={tempCardStyle}>
+                <CardContent>
+                  <h3>Contact</h3>
+                  <p>
+                    Email:
+                    {` ${appData.email}`}
+                  </p>
+                  <p>
+                    Phone:
+                    {` ${appData.phone}`}
+                  </p>
+                </CardContent>
+              </Card>
+              <Card style={tempCardStyle}>
+                <CardContent>
+                  <h3>Food</h3>
+                  <p>
+                    Diet:
+                    {` ${appData.diet}`}
+                  </p>
+                  <p>
+                    Allergies:
+                    {` ${appData.allergies}`}
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
+          </section>
+          <h2>Experience</h2>
+          <h2>Short Answer</h2>
+          <h2>Misc</h2>
+        </div>
+      );
+    }
+
     return (
       <div>
-        {errorMessage}
+        <h1>Error</h1>
+        <p>{errorMessage}</p>
       </div>
     );
   }
@@ -34,6 +125,7 @@ function mapStateToProps(state) {
   return {
     appData: state.app.data,
     errorMessage: state.app.errorMessage,
+    fetching: state.app.fetching,
   };
 }
 
@@ -46,6 +138,7 @@ Application.propTypes = {
   fetchApplications: PropTypes.func.isRequired,
   appData: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   errorMessage: PropTypes.string,
+  fetching: PropTypes.bool.isRequired,
 };
 
 Application.defaultProps = {
