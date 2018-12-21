285ce3f99fb665e7d4de0e65f6dc7f00ceba7f78
Mian Uddin <mianuddin@gmail.com>
2018-12-21 00:07:08 -0800

    feat(application.js): create fetching bool in state

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -3,25 +3,23 @@ import * as types from '../actions/types';
 const INITIAL_STATE = {
   error: false,
   errorMessage: '',
+  fetching: true,
   data: null,
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case types.FETCH_ATTEMPT:
-      return {
-        ...state,
-        errorMessage: '',
-      };
     case types.FETCH_GUCCI:
       return {
         ...state,
         data: action.data,
+        fetching: false,
       };
     case types.FETCH_FAIL:
       return {
         ...state,
         errorMessage: action.error.message,
+        fetching: false,
       };
     default:
       return state;
