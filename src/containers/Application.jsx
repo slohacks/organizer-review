d648b63658190db9d74d8d2269dbf00c09f17f03
Mian Uddin <mianuddin@gmail.com>
2018-12-22 17:35:01 -0800

    feat(Application): add side margin and update grid

--- src/containers/Application.jsx
+++ src/containers/Application.jsx
@@ -60,186 +60,188 @@ class Application extends Component {
       return (
         <div>
           <h1>{appData.name}</h1>
-          <section>
-            <h2>Personal Info</h2>
-            <div className="rowStyle">
-              <Card className="cardStyle">
-                <CardContent>
-                  <h3 className="cardTitle">Demographics</h3>
-                  <List>
-                    <ListItem>
-                      <ListItemText primary="Graduation Date" secondary={parseGradDate(appData.grad_date)} />
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText primary="Gender" secondary={handleUndefinedFieldWithOther(appData.gender, appData.other_gender)} />
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText primary="Ethnicity" secondary={handleUndefinedFieldWithOther(appData.ethnicity, appData.other_ethnicity)} />
-                    </ListItem>
-                  </List>
-                </CardContent>
-              </Card>
-              <Card className="cardStyle">
-                <CardContent>
-                  <h3 className="cardTitle">Travel</h3>
-                  <List>
-                    <ListItem>
-                      <ListItemText primary="College" secondary={handleUndefinedField(appData.college)} />
-                    </ListItem>
-                    <ListItem
-                      button={appData.city !== undefined}
-                      component={appData.city ? 'a' : 'li'}
-                      href={appData.city ? `https://www.google.com/maps/place/${appData.city}` : ''}
+          <div className="sides">
+            <section>
+              <h2>Personal Info</h2>
+              <div className="rowStyle">
+                <Card className="cardStyle">
+                  <CardContent>
+                    <h3 className="cardTitle">Demographics</h3>
+                    <List>
+                      <ListItem>
+                        <ListItemText primary="Graduation Date" secondary={parseGradDate(appData.grad_date)} />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemText primary="Gender" secondary={handleUndefinedFieldWithOther(appData.gender, appData.other_gender)} />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemText primary="Ethnicity" secondary={handleUndefinedFieldWithOther(appData.ethnicity, appData.other_ethnicity)} />
+                      </ListItem>
+                    </List>
+                  </CardContent>
+                </Card>
+                <Card className="cardStyle">
+                  <CardContent>
+                    <h3 className="cardTitle">Travel</h3>
+                    <List>
+                      <ListItem>
+                        <ListItemText primary="College" secondary={handleUndefinedField(appData.college)} />
+                      </ListItem>
+                      <ListItem
+                        button={appData.city !== undefined}
+                        component={appData.city ? 'a' : 'li'}
+                        href={appData.city ? `https://www.google.com/maps/place/${appData.city}` : ''}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <ListItemText primary="City" secondary={handleUndefinedField(appData.city)} />
+                      </ListItem>
+                    </List>
+                  </CardContent>
+                </Card>
+                <Card className="cardStyle">
+                  <CardContent>
+                    <h3 className="cardTitle">Contact</h3>
+                    <List>
+                      <ListItem button component="a" href={`mailto:${appData.email}`}>
+                        <ListItemText primary="Email" secondary={handleUndefinedField(appData.email)} />
+                      </ListItem>
+                      <ListItem
+                        button={appData.phoneNumber !== undefined}
+                        component={appData.phoneNumber ? 'a' : 'li'}
+                        href={appData.phoneNumber ? `tel:${appData.phoneNumber}` : ''}
+                      >
+                        <ListItemText primary="Phone" secondary={handleUndefinedField(appData.phoneNumber)} />
+                      </ListItem>
+                    </List>
+                  </CardContent>
+                </Card>
+                <Card className="cardStyle">
+                  <CardContent>
+                    <h3 className="cardTitle">Food</h3>
+                    <List>
+                      <ListItem>
+                        <ListItemText primary="Diet" secondary={handleUndefinedField(appData.diet)} />
+                      </ListItem>
+                      <ListItemText primary="Allergies" secondary={handleUndefinedField(appData.allergies)} />
+                    </List>
+                  </CardContent>
+                </Card>
+              </div>
+            </section>
+            <section>
+              <h2>Experience</h2>
+              <div className="rowStyle">
+                <Card className="cardStyle">
+                  <CardContent>
+                    <h3 className="cardTitle">Resume</h3>
+                    <List>
+                      <ListItem>
+                        <ListItemText primary="Name" secondary={appData.resume || 'N/A'} />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemText
+                          primary="Size"
+                          secondary={resumeMetadata && resumeMetadata.size
+                            ? `${(parseInt(resumeMetadata.size, 10) / 1024 / 1024).toFixed(2)} mb`
+                            : 'N/A'}
+                        />
+                      </ListItem>
+                    </List>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      size="small"
+                      disabled={!resumeMetadata}
+                      component="a"
+                      href={resumeUrl}
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <ListItemText primary="City" secondary={handleUndefinedField(appData.city)} />
-                    </ListItem>
-                  </List>
-                </CardContent>
-              </Card>
-              <Card className="cardStyle">
-                <CardContent>
-                  <h3 className="cardTitle">Contact</h3>
-                  <List>
-                    <ListItem button component="a" href={`mailto:${appData.email}`}>
-                      <ListItemText primary="Email" secondary={handleUndefinedField(appData.email)} />
-                    </ListItem>
-                    <ListItem
-                      button={appData.phoneNumber !== undefined}
-                      component={appData.phoneNumber ? 'a' : 'li'}
-                      href={appData.phoneNumber ? `tel:${appData.phoneNumber}` : ''}
-                    >
-                      <ListItemText primary="Phone" secondary={handleUndefinedField(appData.phoneNumber)} />
-                    </ListItem>
-                  </List>
-                </CardContent>
-              </Card>
-              <Card className="cardStyle">
-                <CardContent>
-                  <h3 className="cardTitle">Food</h3>
-                  <List>
-                    <ListItem>
-                      <ListItemText primary="Diet" secondary={handleUndefinedField(appData.diet)} />
-                    </ListItem>
-                    <ListItemText primary="Allergies" secondary={handleUndefinedField(appData.allergies)} />
-                  </List>
-                </CardContent>
-              </Card>
-            </div>
-          </section>
-          <section>
-            <h2>Experience</h2>
-            <div className="rowStyle">
-              <Card className="cardStyle">
-                <CardContent>
-                  <h3 className="cardTitle">Resume</h3>
-                  <List>
-                    <ListItem>
-                      <ListItemText primary="Name" secondary={appData.resume || 'N/A'} />
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText
-                        primary="Size"
-                        secondary={resumeMetadata && resumeMetadata.size
-                          ? `${(parseInt(resumeMetadata.size, 10) / 1024 / 1024).toFixed(2)} mb`
-                          : 'N/A'}
-                      />
-                    </ListItem>
-                  </List>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    size="small"
-                    disabled={!resumeMetadata}
-                    component="a"
-                    href={resumeUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Download
-                  </Button>
-                </CardActions>
-              </Card>
-              <Card className="cardStyle">
-                <CardContent>
-                  <h3 className="cardTitle">Sites</h3>
-                  <List>
-                    <ListItem
-                      button={appData.github !== undefined}
-                      component={appData.github ? 'a' : 'li'}
-                      href={appData.github ? `https://github.com/${appData.github}` : ''}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <ListItemText primary="GitHub" secondary={handleUndefinedField(appData.github)} />
-                    </ListItem>
-                    <ListItem
-                      button={appData.linkedin !== undefined}
-                      component={appData.linkedin ? 'a' : 'li'}
-                      href={appData.linkedin ? `https://www.linkedin.com/in/${appData.linkedin}` : ''}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <ListItemText primary="LinkedIn" secondary={handleUndefinedField(appData.linkedin)} />
-                    </ListItem>
-                    <ListItem
-                      button={appData.website !== undefined}
-                      component={appData.website ? 'a' : 'li'}
-                      href={appData.website ? `${appData.website}` : ''}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <ListItemText primary="Personal Website" secondary={handleUndefinedField(appData.website)} />
-                    </ListItem>
-                    <ListItem
-                      button={appData.other_link !== undefined}
-                      component={appData.other_link ? 'a' : 'li'}
-                      href={appData.other_link ? `${appData.other_link}` : ''}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <ListItemText primary="Other Link" secondary={handleUndefinedField(appData.other_link)} />
-                    </ListItem>
-                  </List>
-                </CardContent>
-              </Card>
-            </div>
-          </section>
-          <section>
-            <h2>Short Answer</h2>
-            <div className="rowStyle">
-              <Card className="cardStyle">
-                <CardContent>
-                  <h3 className="cardTitle">Challenge</h3>
-                  <p>
-                    {handleUndefinedField(appData.challenge)}
-                  </p>
-                </CardContent>
-              </Card>
-              <Card className="cardStyle">
-                <CardContent>
-                  <h3 className="cardTitle">Project</h3>
-                  <p>
-                    {handleUndefinedField(appData.project)}
-                  </p>
-                </CardContent>
-              </Card>
-            </div>
-          </section>
-          <section>
-            <h2>Misc</h2>
-            <div className="rowStyle">
-              <Card className="cardStyle">
-                <CardContent>
-                  <h3 className="cardTitle">Other Comments</h3>
-                  <p>
-                    {handleUndefinedField(appData.misc)}
-                  </p>
-                </CardContent>
-              </Card>
-            </div>
-          </section>
+                      Download
+                    </Button>
+                  </CardActions>
+                </Card>
+                <Card className="cardStyle">
+                  <CardContent>
+                    <h3 className="cardTitle">Sites</h3>
+                    <List>
+                      <ListItem
+                        button={appData.github !== undefined}
+                        component={appData.github ? 'a' : 'li'}
+                        href={appData.github ? `https://github.com/${appData.github}` : ''}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <ListItemText primary="GitHub" secondary={handleUndefinedField(appData.github)} />
+                      </ListItem>
+                      <ListItem
+                        button={appData.linkedin !== undefined}
+                        component={appData.linkedin ? 'a' : 'li'}
+                        href={appData.linkedin ? `https://www.linkedin.com/in/${appData.linkedin}` : ''}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <ListItemText primary="LinkedIn" secondary={handleUndefinedField(appData.linkedin)} />
+                      </ListItem>
+                      <ListItem
+                        button={appData.website !== undefined}
+                        component={appData.website ? 'a' : 'li'}
+                        href={appData.website ? `${appData.website}` : ''}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <ListItemText primary="Personal Website" secondary={handleUndefinedField(appData.website)} />
+                      </ListItem>
+                      <ListItem
+                        button={appData.other_link !== undefined}
+                        component={appData.other_link ? 'a' : 'li'}
+                        href={appData.other_link ? `${appData.other_link}` : ''}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <ListItemText primary="Other Link" secondary={handleUndefinedField(appData.other_link)} />
+                      </ListItem>
+                    </List>
+                  </CardContent>
+                </Card>
+              </div>
+            </section>
+            <section>
+              <h2>Short Answer</h2>
+              <div className="rowStyle">
+                <Card className="cardStyle">
+                  <CardContent>
+                    <h3 className="cardTitle">Challenge</h3>
+                    <p>
+                      {handleUndefinedField(appData.challenge)}
+                    </p>
+                  </CardContent>
+                </Card>
+                <Card className="cardStyle">
+                  <CardContent>
+                    <h3 className="cardTitle">Project</h3>
+                    <p>
+                      {handleUndefinedField(appData.project)}
+                    </p>
+                  </CardContent>
+                </Card>
+              </div>
+            </section>
+            <section>
+              <h2>Misc</h2>
+              <div className="rowStyle">
+                <Card className="cardStyle">
+                  <CardContent>
+                    <h3 className="cardTitle">Other Comments</h3>
+                    <p>
+                      {handleUndefinedField(appData.misc)}
+                    </p>
+                  </CardContent>
+                </Card>
+              </div>
+            </section>
+          </div>
         </div>
       );
     }
