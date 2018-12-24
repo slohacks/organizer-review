19e8e006c90a861a99ce8d52fbd60f1d8be3e761
SLO Hacks Bot (from Travis CI) <34457519+slohacksbot@users.noreply.github.com>
2018-12-24 01:24:12 +0000

    Deploy slohacks/organizer-review to github.com/slohacks/organizer-review.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a26fe9b0a5a31b09400259ccaa7d958e.js"
+  "./precache-manifest.080307536f0a27b13d4f90fadcc9116a.js"
 );
 
 workbox.clientsClaim();
