c6b67e47eb48275d110228ec552bbe8adc52eee4
stonewallstan <stanleyarmstrong31@gmail.com>
2018-12-27 00:11:08 -0800

    feat(Decision.jsx): fix filter method
    the filter method is more consistent at filtering the proper applications
    
    \

--- src/components/Decision.jsx
+++ src/components/Decision.jsx
@@ -6,21 +6,18 @@ import CardContent from '@material-ui/core/CardContent';
 import List from '@material-ui/core/List';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { fetchApplicants } from '../actions/index';
 
 class Decision extends Component {
-  componentDidMount() {
-    const { fetchApplicants: fetchApps, applications } = this.props;
-    if (!applications) {
-      fetchApps();
-    }
-  }
+  constructor(props) {
+    super(props);
+    this.getCSV = this.getCSV.bind(this);
+  };
 
   getCSV = (status) => {
     const { applications } = this.props;
     let csvContent = 'data:text/csv;charset=utf-8, ';
     applications.forEach((application) => {
-      if (application.status === status) {
+      if (application.status === status.target.innerHTML) {
         csvContent += `${application.email},${application.name.split(' ')[0]},${application.name.split(' ')[1]}\r\n`;
       }
     });
@@ -35,13 +32,13 @@ class Decision extends Component {
           <CardContent>
             <h3 className="cardTitle">Decisions</h3>
             <List>
-              <Button variant="outlined" color="primary" onClick={e => this.getCSV(e.target.value)} value="Accepted">
+              <Button variant="outlined" color="primary">
                 <CircularProgress />
               </Button>
-              <Button variant="outlined" onClick={e => this.getCSV(e.target.value)} value="Waitlisted">
+              <Button variant="outlined">
                 <CircularProgress />
               </Button>
-              <Button variant="outlined" color="secondary" onClick={e => this.getCSV(e.target.value)} value="Rejected">
+              <Button variant="outlined" color="secondary">
                 <CircularProgress />
               </Button>
             </List>
@@ -54,13 +51,27 @@ class Decision extends Component {
         <CardContent>
           <h3 className="cardTitle">Decisions</h3>
           <List>
-            <Button variant="outlined" color="primary" onClick={e => this.getCSV(e.target.value)} value="Accepted">
+            <Button
+              variant="outlined"
+              color="primary"
+              value="Accepted"
+              onClick={e => this.getCSV(e)}
+            >
               Accepted
             </Button>
-            <Button variant="outlined" onClick={e => this.getCSV(e.target.value)} value="Waitlisted">
+            <Button
+              variant="outlined"
+              value="Waitlisted"
+              onClick={e => this.getCSV(e)}
+            >
               Waitlist
             </Button>
-            <Button variant="outlined" color="secondary" onClick={e => this.getCSV(e.target.value)} value="Rejected">
+            <Button
+              variant="outlined"
+              color="secondary"
+              value="Rejected"
+              onClick={e => this.getCSV(e)}
+            >
               Rejected
             </Button>
           </List>
@@ -76,7 +87,6 @@ class Decision extends Component {
   }
 }
 Decision.propTypes = {
-  fetchApplicants: PropTypes.func.isRequired,
   applications: PropTypes.arrayOf(PropTypes.shape({})),
 };
 Decision.defaultProps = {
@@ -89,5 +99,5 @@ function mapStateToProps(state) {
 }
 export default connect(
   mapStateToProps,
-  { fetchApplicants },
+  null,
 )(Decision);
