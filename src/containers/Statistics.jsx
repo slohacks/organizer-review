1b8cb0737c3f067ec6bf1d66a53fa9294fbe3650
stonewallstan <stanleyarmstrong31@gmail.com>
2018-12-28 01:54:59 -0800

    feat(Statistics.jsx/css): add layout of Statistics page

--- src/containers/Statistics.jsx
+++ src/containers/Statistics.jsx
@@ -4,6 +4,11 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import requireAuth from '../components/requireAuth';
 import Decision from '../components/statistics/Decision';
+import Majors from '../components/statistics/Majors';
+import Colleges from '../components/statistics/Colleges';
+import Gender from '../components/statistics/Gender';
+import Ethnicity from '../components/statistics/Ethnicity';
+import './Statistics.css';
 
 const Statistics = () => {
   return (
@@ -16,8 +21,12 @@ const Statistics = () => {
         </Toolbar>
       </AppBar>
       <div className="sides">
-        <section>
+        <section className="stats">
           <div className="rowStyle">
+            <Colleges />
+            <Majors />
+            <Ethnicity />
+            <Gender />
             <Decision />
           </div>
         </section>
