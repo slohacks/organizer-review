6d99c6399623558a71a229043f00f33e8d108118
stonewallstan <stanleyarmstrong31@gmail.com>
2018-12-28 01:52:53 -0800

    feat(statistics): add statistic cards
    statistics included are majors, gender, ethnicity, and colleges

---
+++ src/components/statistics/Gender.jsx
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
+
+const styles = {
+  expand: {
+    transform: 'rotate(0deg)',
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+};
+
+class Gender extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+    this.handleExpansion = this.handleExpansion.bind(this);
+    this.getCSV = this.getCSV.bind(this);
+  }
+
+  handleExpansion = () => {
+    const { expanded } = this.state;
+    this.setState(({ expanded: !expanded }));
+  }
+
+  getGenderCounts = (applications) => {
+    const counts = [];
+
+    applications.forEach((application) => {
+      const count = counts.find(el => el.gender === application.gender);
+
+      if (count !== undefined) {
+        count.count += 1;
+      } else {
+        counts.push({ gender: application.gender, count: 1 });
+      }
+    });
+
+    return counts.sort((a, b) => b.count - a.count);
+  }
+
+  getCSV = () => {
+    const { applications } = this.props;
+    const counts = this.getGenderCounts(applications);
+    let csvContent = 'data:text/csv;charset=utf-8,Status,Count\r\n';
+
+    counts.forEach((count) => {
+      csvContent += `${count.major.replace(/,/g, '-').trim()},${count.count}\r\n`;
+    });
+
+    csvContent = encodeURI(csvContent);
+    const link = document.createElement('a');
+    link.setAttribute('href', csvContent);
+    link.setAttribute('download', 'Genders.csv');
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
+
+  render() {
+    const { applications } = this.props;
+    const { expanded } = this.state;
+    const counts = this.getGenderCounts(applications);
+
+    return (
+      <Card className="cardStyle">
+        <CardContent>
+          <h3 className="cardTitle">Genders</h3>
+          <List>
+            {counts.slice(0, 4).map(count => (
+              <ListItem
+                key={count.gender}
+              >
+                <ListItemText
+                  primary={count.gender}
+                  secondary={count.count}
+                />
+              </ListItem>
+            ))}
+            <Collapse
+              in={expanded}
+              timeout="auto"
+              unmountOnExit
+            >
+              {counts.slice(4).map(count => (
+                <ListItem
+                  key={count.gender}
+                >
+                  <ListItemText
+                    primary={count.gender}
+                    secondary={count.count}
+                  />
+                </ListItem>
+              ))}
+            </Collapse>
+          </List>
+        </CardContent>
+        <CardActions>
+          <Button
+            color="primary"
+            onClick={this.getCSV}
+          >
+            Get CSV
+          </Button>
+          <IconButton
+            style={expanded ? styles.expand : styles.expandOpen}
+            onClick={this.handleExpansion}
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    );
+  }
+}
+Gender.propTypes = {
+  applications: PropTypes.arrayOf(PropTypes.shape({})),
+};
+Gender.defaultProps = {
+  applications: [],
+};
+function mapStateToProps(state) {
+  return {
+    applications: state.apps.data,
+  };
+}
+export default connect(
+  mapStateToProps,
+  null,
+)(Gender);
